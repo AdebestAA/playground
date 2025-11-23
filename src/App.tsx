bdff3dc62336ccbc4967b60d4a9bbb74f77ac9da
AdebestAA <aadebesta@gmail.com>
2025-11-23 17:20:20 +0100

    commented out

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 
 import './index.css'
-import Slides from './components/slides'
+// import Slides from './components/slides'
 import SplitTextComponent from './components/split-text'
 import Stagger from './components/gsap/stagger'
 import Timeline from './components/gsap/timeline'
 import Play from './components/gsap/play'
 import CursorEffect from './components/cursor-effect'
-import SplitTextCountries from './components/split-text-countries'
+// import SplitTextCountries from './components/split-text-countries'
 import Welcome from './components/welcome'
 
 
